3919dc2572559b4b7ca02a8005dad5190203482a
Shqiprim Veseli <shqiprimveseli1122@gmail.com>
2025-06-27 20:44:41 +0200

    Some of the getters for the home page

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\RoleMiddleware;
 use App\Http\Controllers\WorkoutController;
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\MealPlanController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\PostController;
 use App\Http\Controllers\OpenRouterController;
@@ -37,7 +38,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
 });
 Route::post('/generate-workout', [WorkoutController::class, 'generateWorkoutPlan']);
 Route::get('/user-workouts', [WorkoutController::class, 'getAllUserWorkouts']);
-Route::post('/generate-diet', [FoodController::class, 'generateDiet']);
+
+
+Route::post('/generate-diet', [MealPlanController::class, 'generateDiet']);
+Route::get('/get-user-diet', [MealPlanController::class, 'getFullDiet']);
+
 
 
 
